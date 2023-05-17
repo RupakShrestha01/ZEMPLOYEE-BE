7147dea8283c4d39691ae3b75f6222c5615be13a
Rupak shrestha <rupakshrestha2002@gmail.com>
2023-05-17 16:42:22 +0545

    Add initial dummie Employee data.

--- grails-app/init/zemployee/BootStrap.groovy
+++ grails-app/init/zemployee/BootStrap.groovy
@@ -3,6 +3,51 @@ package zemployee
 class BootStrap {
 
     def init = { servletContext ->
+    new Employee("firstName" : "Rupak",
+"lastName" : "Shrestha",
+"designation" : "Intern",
+"gender" : "Male",
+"nationality" : "Nepali",
+"phoneNumber" : "9814722817",
+"address" : "Matatirtha-4,KTM",
+"email": "rupakshrestha2002@gmail.com"
+
+
+).save()
+new Employee("firstName" : "Akash",
+"lastName" : "Baniya",
+"designation" : "Web Dev",
+"gender" : "Male",
+"nationality" : "Nepali",
+"phoneNumber" : "9814722817",
+"address" : "Matatirtha-4,KTM",
+"email": "rupakshrestha2002@gmail.com"
+
+
+).save()
+
+new Employee("firstName" : "Sunita",
+"lastName" : "Baram",
+"designation" : "Web Designer",
+"gender" : "Female",
+"nationality" : "Nepali",
+"phoneNumber" : "9814722817",
+"address" : "Matatirtha-4,KTM",
+"email": "rupakshrestha2002@gmail.com"
+
+
+).save()
+new Employee("firstName" : "Dristi",
+"lastName" : "Sigdel",
+"designation" : "Data Analyst",
+"gender" : "Female",
+"nationality" : "Nepali",
+"phoneNumber" : "9814722817",
+"address" : "Matatirtha-4,KTM",
+"email": "rupakshrestha2002@gmail.com"
+
+
+).save()
     }
     def destroy = {
     }
