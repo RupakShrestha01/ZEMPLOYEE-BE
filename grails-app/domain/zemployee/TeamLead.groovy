352e0c647a8b881174a3e929ffed3ad02d2047bf
Rupak shrestha <rupakshrestha2002@gmail.com>
2023-06-02 15:27:05 +0545

    create a designation attribute in Teamlead domaim

--- grails-app/domain/zemployee/TeamLead.groovy
+++ grails-app/domain/zemployee/TeamLead.groovy
@@ -1,12 +1,13 @@
 package zemployee 
-
-
 class TeamLead {
     String name
+    String designation
 
     static belongsTo = [team: Team]
 
     static constraints = {
         name nullable: false, blank: false
+        designation nullable: false
     }
+
 }
