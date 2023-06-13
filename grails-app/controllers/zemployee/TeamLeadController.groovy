7ad54f4d101bc2f763f33466df8a730595c9be16
Rupak shrestha <rupakshrestha2002@gmail.com>
2023-06-13 13:55:47 +0545

    Create a action for getting custom json Teamlead

--- grails-app/controllers/zemployee/TeamLeadController.groovy
+++ grails-app/controllers/zemployee/TeamLeadController.groovy
@@ -1,99 +1,28 @@
 package zemployee
-
-import grails.validation.ValidationException
-import static org.springframework.http.HttpStatus.*
-
+import grails.scaffolding.*
+import grails.converters.JSON
 class TeamLeadController {
-
+    static scaffold = TeamLead
     TeamLeadService teamLeadService
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    def ListAll(){
+        def results =TeamLead.list()
+        
+        def teamLeadList = results.collect { teamlead ->
+            [
+                teamlead : teamlead.firstName,
+                team: teamlead.team.name,
+                designation: teamlead.designation,
+                    // Include other team attributes here
+        
+                employee: [
+                    teamlead.employee
+                ]
+            ]
+         }
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond teamLeadService.list(params), model:[teamLeadCount: teamLeadService.count()]
+        render teamLeadList as JSON
+    }
     }
 
-    def show(Long id) {
-        respond teamLeadService.get(id)
-    }
 
-    def create() {
-        respond new TeamLead(params)
-    }
-
-    def save(TeamLead teamLead) {
-        if (teamLead == null) {
-            notFound()
-            return
-        }
-
-        try {
-            teamLeadService.save(teamLead)
-        } catch (ValidationException e) {
-            respond teamLead.errors, view:'create'
-            return
-        }
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'teamLead.label', default: 'TeamLead'), teamLead.id])
-                redirect teamLead
-            }
-            '*' { respond teamLead, [status: CREATED] }
-        }
-    }
-
-    def edit(Long id) {
-        respond teamLeadService.get(id)
-    }
-
-    def update(TeamLead teamLead) {
-        if (teamLead == null) {
-            notFound()
-            return
-        }
-
-        try {
-            teamLeadService.save(teamLead)
-        } catch (ValidationException e) {
-            respond teamLead.errors, view:'edit'
-            return
-        }
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'teamLead.label', default: 'TeamLead'), teamLead.id])
-                redirect teamLead
-            }
-            '*'{ respond teamLead, [status: OK] }
-        }
-    }
-
-    def delete(Long id) {
-        if (id == null) {
-            notFound()
-            return
-        }
-
-        teamLeadService.delete(id)
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'teamLead.label', default: 'TeamLead'), id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'teamLead.label', default: 'TeamLead'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
-}
